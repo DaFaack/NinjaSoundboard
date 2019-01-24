3ec08374055ef5d835c1fa077106657a17faa28d
Aaron <aaron.wtal@googlemail.com>
2019-01-24 15:10:16 +0100

    Für BlackSkyStudios geupdated

--- app/src/main/java/com/blackskystudios/soundboardninja/MainActivity.java
+++ app/src/main/java/com/blackskystudios/soundboardninja/MainActivity.java
@@ -123,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
                 if(menuItem.getItemId() == R.id.teilen){
                     Intent shareIntent = new Intent(Intent.ACTION_SEND);
                     shareIntent.setType("text/plain");
-                    shareIntent.putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=com.greenwoodsproductions.soundboardninja");
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=com.blackskystudios.soundboardninja");
                     startActivity(Intent.createChooser(shareIntent,  "Share via..."));
                 }
 
